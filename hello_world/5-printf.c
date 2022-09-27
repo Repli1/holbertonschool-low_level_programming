654959afa6ae0b6d6e1924edb00078272b904301
Repli1 <5724@holbertonstudents.com>
2022-09-27 09:55:56 -0700

    added comments

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Program that prints the following line
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
