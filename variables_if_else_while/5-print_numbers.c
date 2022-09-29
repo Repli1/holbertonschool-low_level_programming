2c849e5107a6def481fc5dff0e8437c17be619a3
Repli1 <5724@holbertonstudents.com>
2022-09-29 06:34:22 -0700

    added file

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Program that prints the following line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf('%d\n', 0123456789);
+	return (0);
+}
