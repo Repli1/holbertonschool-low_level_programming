b8e43e2be1e4bf29f4f1335ac6be34e1df9811fc
Repli1 <5724@holbertonstudents.com>
2022-09-29 07:37:14 -0700

    added file

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Program that prints the following line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z'; c++)
+		if (c != q || c != e)
+			putchar(c);
+	return (0);
+}
