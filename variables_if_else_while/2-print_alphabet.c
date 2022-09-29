363ffe4d75a74520464f27ab7adb0c21236fecb8
Repli1 <5724@holbertonstudents.com>
2022-09-29 05:29:54 -0700

    added file

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+
+/**
+ * main - Entry point
+ *
+ * Description: Program that prints the following line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+}
