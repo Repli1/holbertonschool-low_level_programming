fbbd3445013befe752fb65bca9c1b690b3ff718a
Repli1 <5724@holbertonstudents.com>
2022-09-29 05:18:49 -0700

    fixed syntax

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,7 +3,13 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: Program that prints the following line
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -12,16 +18,19 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	printf("Last digit of ");
 	printf("%d", n);
-	printf("is ");
+	printf(" is ");
 	printf("%d", n % 10);
-	if (n > 5) {
-		printf(" and is greater than 5");
+	if (n > 5)
+	{
+		printf(" and is greater than 5\n");
 	}
-	else if (n == 0){
-		printf(" and is 0");
+	else if (n == 0)
+	{
+		printf(" and is 0\n");
 	}
-	else if (n < 6 && n != 0) {
-		printf(" and is less than 6 and not 0");
+	else if (n < 6 && n != 0)
+	{
+		printf(" and is less than 6 and not 0\n");
 	}
 	return (0);
 }
