5d0902949b5ceaaa415cd8253eabcaf122730e97
Repli1 <5724@holbertonstudents.com>
2022-09-29 08:46:25 -0700

    added file

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Program that prints the following line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 48;
+
+	char ch = 'a';
+
+		for (c = 48; c <= 58; c++)
+			putchar(c);
+		for (ch = 'a'; ch <= 'f'; c++)
+			putchar(ch);
+		putchar('\n');
+		return (0);
+}
