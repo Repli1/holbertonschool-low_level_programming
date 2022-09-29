ed74febd27f881b6fc4fe18645135677063a68d9
Repli1 <5724@holbertonstudents.com>
2022-09-29 09:00:27 -0700

    fixed everything

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Description: Program that prints the following line
- *     *
- *      * Return: Always 0 (Success)
- *       */
+ * main - Entry point
+ *
+ * Description: Program that prints the following line
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		int c = 'a';
+	int c = 48;
 
-			for (c = 0; c <= 9; c++)
-						putchar(c);
-				putchar('\n');
-					return (0);
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
