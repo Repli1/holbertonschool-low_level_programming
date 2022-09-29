c6124872319f108e562b78778759ac645065eb82
Repli1 <5724@holbertonstudents.com>
2022-09-29 08:52:07 -0700

    added file

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Description: Program that prints the following line
+ *     *
+ *      * Return: Always 0 (Success)
+ *       */
+int main(void)
+{
+		char c = 'a';
+
+			for (c = 'a'; c <= 'z'; c++)
+						putchar(c);
+				putchar('\n');
+					return (0);
+}
