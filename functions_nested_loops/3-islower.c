864f599c1eb29ea3a743e35f7d7ec677b98a9aec
Repli1 <5724@holbertonstudents.com>
2022-09-30 09:13:42 -0700

    fixed logical error

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,14 +2,14 @@
 /**
  *  _islower - Entry point
  *
- * Description: Program that prints the following line
+ * @c: i'ts the variable!
  *
  * Return: 1 or 0 (Success)
  */
 
 int _islower(int c)
 {
-	if ('a' <= c <= 'z')
+	if ('a' <= c && c <= 'z')
 		return (1);
 	else
 		return (0);
