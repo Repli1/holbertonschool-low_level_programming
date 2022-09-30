b6bfe28f28bc54da15b2bf8aa9c6049297ca63dd
Repli1 <5724@holbertonstudents.com>
2022-09-30 09:54:09 -0700

    added files

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * @int: Variable (Confusing)
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int)
+{
+	if (int > 0)
+		return (int);
+	else if (int == 0)
+		return (0);
+	else
+		return (-int);
+}
