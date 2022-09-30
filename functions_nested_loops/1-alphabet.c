2caf93a576136396f170fa29c56f0faaff07d5c8
Repli1 <5724@holbertonstudents.com>
2022-09-30 06:51:14 -0700

    i have no idea

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Program that prints the following line
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c = 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
