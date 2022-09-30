31f5b6c3941d5a356043a3cd2bd6bfe21ee77740
Repli1 <5724@holbertonstudents.com>
2022-09-30 09:55:33 -0700

    named variable to test

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,17 +2,17 @@
 /**
  * print_alphabet_x10 - Entry point
  *
- * @int: Variable (Confusing)
+ * @n: Variable
  *
  * Return: Always 0 (Success)
  */
 
-int _abs(int)
+int _abs(int n)
 {
-	if (int > 0)
-		return (int);
-	else if (int == 0)
+	if (n > 0)
+		return (n);
+	else if (n == 0)
 		return (0);
 	else
-		return (-int);
+		return (-n);
 }
