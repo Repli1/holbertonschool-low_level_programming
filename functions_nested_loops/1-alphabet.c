73ad8d0a5036547ed43ba6e445d2f1874b61691c
Repli1 <5724@holbertonstudents.com>
2022-09-30 06:57:55 -0700

    fixed syntax

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  *
  * Description: Program that prints the following line
  *
  * Return: Always 0 (Success)
  */
+
 void print_alphabet(void)
 {
 	char c;
