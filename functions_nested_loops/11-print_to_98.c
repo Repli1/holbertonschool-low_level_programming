cc48580010f532ecddd55ba6e83f7b332f4af105
Repli1 <5724@holbertonstudents.com>
2022-10-03 08:13:26 -0700

    added two files

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_to_98 - Entry point
+ *
+ * @n: It's a variable!
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_to_98(int n)
+{
+	while (n > 98)
+	{
+		printf ("%d", n);
+		n--;
+		_putchar(',');
+		_putchar(' ');
+	}
+	while (n < 98)
+		printf ("%d", n);
+		n++;
+		_putchar(',');
+		_putchar(' ');
+	if (n == 98)
+		printf ("%d", n);
+}
