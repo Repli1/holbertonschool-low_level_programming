72df0b0afed84da8810c9a864844400f6de434c3
Repli1 <5724@holbertonstudents.com>
2022-10-03 05:08:30 -0700

    added two files

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: Program that prints the following line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_last_digit(int n)
+{
+	_putchar(n%10);
+}
