0fa17ff1d5409822940f97463f61087d87cffaf7
Repli1 <5724@holbertonstudents.com>
2022-10-03 08:17:30 -0700

    fixed syntax

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,16 +12,18 @@ void print_to_98(int n)
 {
 	while (n > 98)
 	{
-		printf ("%d", n);
+		printf("%d", n);
 		n--;
 		_putchar(',');
 		_putchar(' ');
 	}
 	while (n < 98)
-		printf ("%d", n);
+	{
+		printf("%d", n);
 		n++;
 		_putchar(',');
 		_putchar(' ');
+	}
 	if (n == 98)
-		printf ("%d", n);
+		printf("%d", n);
 }
