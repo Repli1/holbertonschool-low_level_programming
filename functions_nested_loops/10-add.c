054352f2f81ae7c33f24f4d887e26172e7648302
Repli1 <5724@holbertonstudents.com>
2022-10-03 07:12:00 -0700

    added files

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Entry point
+ *
+ * Description: Program that prints the following line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -13,3 +13,4 @@ int _abs(int n);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
