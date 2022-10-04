9459e6b8adea8396167b04217fe78a1939838ae0
Repli1 <5724@holbertonstudents.com>
2022-10-04 05:18:31 -0700

    added file

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - Entry point
+ *
+ * @c: It's a character!
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+		return (1);
+	return (0);
+}
