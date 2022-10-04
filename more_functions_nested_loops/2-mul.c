e29497eb5ddae5078ab685c61526ed43b16fc30e
Repli1 <5724@holbertonstudents.com>
2022-10-04 05:36:15 -0700

    added file

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - Entry point
+ *
+ * @a: It's an integer!
+ *
+ * @b: It's another integer!
+ *
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
