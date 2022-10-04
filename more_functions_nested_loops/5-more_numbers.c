bc91334b7fe2d170888c7f449b4f0c378828029c
Repli1 <5724@holbertonstudents.com>
2022-10-04 06:09:53 -0700

    added two files

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * more_numbers - Entry point
+ *
+ * Description: A program that prints the following line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+void more_numbers(void)
+{
+	char c = 48;
+
+	int n, m;
+
+	for (m = 0; m <= 10, m++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+				_putchar(49);
+			_putchar(c);
+			c++;
+			if (c == 58)
+				c = 48;
+		}
+		c = 48;
+		_putchar('\n');
+	}
+}
