11aa52ecc94c107e616c0a76b77d9633d3a201f6
Repli1 <5724@holbertonstudents.com>
2022-10-04 07:14:39 -0700

    added file

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints the following line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3) == 0)
+			printf("Fizz");
+		if ((n % 5) == 0)
+			printf("Buzz");
+		if (!(((n % 3) == 0) || ((n % 5) == 0)))
+			printf("%d", n);
+		printf("%c", ' ');
+	}
+	printf("%c", '\n');
+	return (0);
+}
