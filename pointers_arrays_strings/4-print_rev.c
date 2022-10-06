b25d7cb296f6e596c11ed57d79fba9b8cb6b69af
Repli1 <5724@holbertonstudents.com>
2022-10-06 09:29:24 -0700

    added file

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - Entry point
+ *
+ * @s: It's a string!
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_rev(char *s)
+{
+	int n = 0;
+
+	for (n = 0; s[n] != '\0'; n++)
+	{
+		_putchar(s[-n]);
+	}
+
+	_putchar('\n');
+}
