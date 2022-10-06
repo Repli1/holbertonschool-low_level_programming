ecce1f35660ad0fcca7a1d13575c1e3c82f4f0ae
Repli1 <5724@holbertonstudents.com>
2022-10-06 07:25:21 -0700

    added file

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * swap_int - Entry point
+ *
+ * @a: It's an integer!
+ *
+ * @b: It's another integer!
+ *
+ * Return: Always 0 (Success)
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = a;
+
+	a = b;
+	b = c;
+}
