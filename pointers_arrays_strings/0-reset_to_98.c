f470881d540c931d168c587733b7ff0d46838ef0
Repli1 <5724@holbertonstudents.com>
2022-10-06 07:15:48 -0700

    added two files

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - Entry point
+ *
+ * @n: It's an integer!
+ *
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n);
+{
+	n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#include <unistd.h>
+#ifndef main_h
+#define main_h
+#endif
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
