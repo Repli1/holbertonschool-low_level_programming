dedc924aae8827316d0dd8c52a5171361c6a6c88
Repli1 <5724@holbertonstudents.com>
2022-10-10 06:35:53 -0700

    betty

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -17,10 +17,10 @@ void puts_half(char *str)
 		len++;
 
 	if (len % 2 == 0 && len != 0)
-		_putchar(str[len/2]);
+		_putchar(str[len / 2]);
 
-	for (n = 1; str[len/2 + n] != '\0' && str[len/2 + n - 1] != '\0'; n++)
-		_putchar(str[n + len/2]);
+	for (n = 1; str[len / 2 + n] != '\0' && str[len / 2 + n - 1] != '\0'; n++)
+		_putchar(str[n + len / 2]);
 
 	_putchar('\n');
 }
