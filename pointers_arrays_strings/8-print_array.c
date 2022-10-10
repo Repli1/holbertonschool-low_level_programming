dc7695de26522a9067a75588fec8433fcd9cc81e
Repli1 <5724@holbertonstudents.com>
2022-10-10 06:58:18 -0700

    fix

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -14,10 +14,13 @@ void print_array(int *a, int n)
 {
 	int m;
 
-	for (m = 0; m != n; m++)
-	{
-		printf("%d", a[m]);
-		printf("%c", ',');
-		printf("%c", ' ');
-	}
+	if (n > 0)
+		for (m = 0; m != n; m++)
+		{
+			printf("%d", a[m]);
+			if (m != (n - 1))
+				printf("%c", ',');
+				printf("%c", ' ');
+		}
+	_putchar ('\n');
 }
