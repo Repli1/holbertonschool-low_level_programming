7ded63ba57c2bca3c6a690ee5dbf65dde17003eb
Repli1 <5724@holbertonstudents.com>
2022-10-10 06:32:48 -0700

    finaltest

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -16,6 +16,9 @@ void puts_half(char *str)
 	while (str[len] != '\0')
 		len++;
 
+	if (len % 2 == 1)
+		_putchar(str[len/2]);
+
 	for (n = 1; str[len/2 + n] != '\0' && str[len/2 + n - 1] != '\0'; n++)
 		_putchar(str[n + len/2]);
 
