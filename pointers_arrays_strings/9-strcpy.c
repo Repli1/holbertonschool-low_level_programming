4261ba78faa3f90578a4a671314719004821fcd9
Repli1 <5724@holbertonstudents.com>
2022-10-10 07:38:26 -0700

    added file

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcpy - Entry point
+ *
+ * @dest: It's a string!
+ *
+ * @src: It's another string!
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strcpy(char *dest, char *src);
+{
+	int n;
+
+	for (n = 0; src[n] <= '\0'; n++)
+	{
+		dest[n] = src[n];
+	}
+	return (char *dest);
+}
