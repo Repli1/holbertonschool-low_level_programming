ed25f13a8a0af57c3181c011ca20f31d9baa4147
Repli1 <5724@holbertonstudents.com>
2022-10-10 05:45:43 -0700

    added file

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - Entry point
+ *
+ * @str: It's a string!
+ *
+ * Return: Always 0 (Success)
+ */
+
+void puts2(char *str)
+{
+	int n;
+
+	for (n = 0; str[(2 * n) + 1] != '\0' || str[(2 * n)] != '\0'; n++)
+		_putchar(str[(2 * n) + 1]);
+	_putchar('\n');
+}
