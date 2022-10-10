f88888630fd7b2069d83fc607fa16084d7a6c84d
Repli1 <5724@holbertonstudents.com>
2022-10-10 06:51:13 -0700

    added file

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - Entry point
+ *
+ * @a: It's an array!
+ *
+ * @n: It's an integer!
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_array(int *a, int n)
+{
+	int m;
+
+	for (m = 0; m == n; m++)
+	{
+		printf(a[m]);
+		printf(',');
+		printf(' ');
+	}
+}
