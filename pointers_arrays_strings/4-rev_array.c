bace4f4c647e4ca9435dcc0a49a0c31243a1ee5a
Repli1 <5724@holbertonstudents.com>
2022-10-11 06:13:09 -0700

    added file

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * reverse_array - Entry point
+ *
+ * @a: It's an array!
+ *
+ * @n: It's another integer!
+ *
+ * Return: Always 0 (Success)
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	int m;
+
+	for (i = 0; i != n; i++)
+	{
+		m = a[i];
+		a[i] = a[n - i];
+		a[n - i] = m;
+	}
+}
