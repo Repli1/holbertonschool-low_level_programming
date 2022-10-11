549e7d3d0ef052286ac173a9d4003281f0ccaa59
Repli1 <5724@holbertonstudents.com>
2022-10-11 06:54:18 -0700

    added file

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * cap_string - Entry point
+ *
+ * @s: It's a string!
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] <= 122 && s[i] >= 97)
+			if (s[i - 1] != ' ' && s[i - 1] != ',' && s[i - 1] != '.')
+				if (s[i - 1] != '\n' && s[i - 1] != ';' && s[i - 1] != '"')
+					if (s[i - 1] != '?' && s[i - 1] != '!' && s[i - 1] != 9)
+						if (s[i - 1] != '(' && s[i - 1] != ')' && s[i - 1] != '{')
+							if (s[i - 1] != '}')
+								s[i] = s[i] - 32;
+	return (s);
+}
