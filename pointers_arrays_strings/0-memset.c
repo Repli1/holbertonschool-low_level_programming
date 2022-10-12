c8d64e41fef06f067159e9a1ab45ea438580ec0e
Repli1 <5724@holbertonstudents.com>
2022-10-12 04:55:18 -0700

    added file

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _memset - Entry point
+ *
+ * @s: It's a string!
+ *
+ * @b: It's a character!
+ *
+ * @n: It's an integer!
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
