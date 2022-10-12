b7ec3364054207f94965c08f5dc6b6b3d3b585ab
Repli1 <5724@holbertonstudents.com>
2022-10-12 05:04:26 -0700

    added file

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _memcpy - Entry point
+ *
+ * @src: It's a string!
+ *
+ * @dest: It's another string!
+ *
+ * @n: It's an unsigned integer!
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i]
+	}
+	return (dest);
+}
