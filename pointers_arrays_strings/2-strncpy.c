83f0a3c17aff05ab4bfdb01a976116e7b03fc3bd
Repli1 <5724@holbertonstudents.com>
2022-10-13 08:53:29 -0700

    fix!

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -17,10 +17,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	int len = 0;
 
-	while (dest[len] != '\0')
-		len++;
-
-	for (i = 0; i <	n; i++)
+	for (i = 0; (i < n) && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
 	return (dest);
