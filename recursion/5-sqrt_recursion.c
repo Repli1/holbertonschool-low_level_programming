e6fcdc02101bf2eb7a24ff6fa7606996e2cdc1bc
Repli1 <5724@holbertonstudents.com>
2022-10-14 09:31:21 -0700

    added file

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - Entry point
+ *
+ * @n: It's an integer!
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _sqrt_recursion(int n);
+{
+	if (n > 0)
+		if (1 + _sqrt_recursion(n - 1) % n != 0)
+		{
+			return (1 + _sqrt_recursion(n - 1));
+		}
+		else
+			return (n);
+	else if (n == 0)
+		return (0);
+	else
+		return (-1);
+}
