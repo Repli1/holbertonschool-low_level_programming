5caa99ce2811cadc68abe0cda70124fae53d0361
Repli1 <5724@holbertonstudents.com>
2022-10-14 08:14:23 -0700

    final test

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -6,17 +6,12 @@
  *
  * Return: Always 0 (Success)
  **/
-int _length(char *s)
-{
-	if (s[0] != '\0')
-		return (1 + _length(s + 1));
-	else
-		return (0);
-}
 
 void _print_rev_recursion(char *s)
 {
-	_putchar(s[_length(s) - 1]);
-	_length(s)--;
-	_print_rev_recursion(s);
+	if (s[0] == '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(s[0]);
+	}
 }
