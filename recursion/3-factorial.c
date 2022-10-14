79b0f13c4988938fb90573ccced6e127169f43ac
Repli1 <5724@holbertonstudents.com>
2022-10-14 08:27:50 -0700

    added file

---
+++ recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * factorial - Entry point
+ *
+ * @n: It's an integer!
+ *
+ * Return: Always 0 (Success)
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n));
+}
