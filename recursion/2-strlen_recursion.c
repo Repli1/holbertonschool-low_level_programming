45acf6210982d1ab3bd8fb722a0df7cc651227a3
Repli1 <5724@holbertonstudents.com>
2022-10-14 08:22:20 -0700

    added file

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Entry point
+ *
+ * @s: It's a string!
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (s[0] != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+		return (0);
+}
