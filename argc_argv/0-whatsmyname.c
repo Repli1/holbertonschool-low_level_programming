6e561fa706decd79addda9c97bd4446e62ce8d6d
Repli1 <5724@holbertonstudents.com>
2022-10-17 09:10:37 -0700

    added files

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * @argc: Argument count.
+ *
+ * @argv: Argument array.
+ *
+ * Return: 0 (Success)
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
---
+++ argc_argv/main.h
@@ -0,0 +1,7 @@
+#include <unistd.h>
+#ifndef main_h
+#define main_h
+#endif
+
+int _putchar(char c);
+int name(char* argv[]);
