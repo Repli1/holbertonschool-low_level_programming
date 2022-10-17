b1a3515d9b60a0835866aae217ba93f2a082ee4e
Repli1 <5724@holbertonstudents.com>
2022-10-17 09:19:15 -0700

    added file

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * @argc: Argument count.
+ *
+ * @argv: Argument array.
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
