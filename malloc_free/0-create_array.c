073f2004f3ced13a965fbbe2cb7941198e13eb2b
Repli1 <5724@holbertonstudents.com>
2022-10-24 12:39:46 -0700

    added files

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * create_array - Entry point
+ *
+ * @size: It's an unsigned int!
+ *
+ * @c: It's a character!
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size = 0)
+		return (NULL);
+
+	char* array[size] = malloc(size * sizeof(char));
+
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
+	return (array[0]);
+}
