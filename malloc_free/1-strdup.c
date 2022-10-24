94e03ebadf983921a3b415c4627a0dada9fb635b
Repli1 <5724@holbertonstudents.com>
2022-10-24 13:22:39 -0700

    added file

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - Entry point
+ *
+ * @str: It's a string!
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strdup(char *str)
+{
+	char *dup = malloc(sizeof(str));
+
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		dup[i] = str[i];
+
+	return (dup);
+}
