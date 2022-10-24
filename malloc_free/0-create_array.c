146287b3063892421ea222455a1b75aa7add2761
Repli1 <5724@holbertonstudents.com>
2022-10-24 12:46:01 -0700

    test2

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,14 +12,14 @@
 
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
-		return (NULL);
-
 	char* array = (char*) malloc(size * sizeof(char));
 
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 		array[i] = c;
-	return (&array);
+	return (array);
 }
