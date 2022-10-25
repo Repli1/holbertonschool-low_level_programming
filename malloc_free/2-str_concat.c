3cc1accd71f64ed93ca415c61c20a61b9ce3c4b8
Repli1 <5724@holbertonstudents.com>
2022-10-24 20:34:43 -0700

    fix

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,6 +16,13 @@ char *str_concat(char *s1, char *s2)
 
 	int i, j;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (str == NULL)
+		return (NULL);
+
 	for (i = 0; s1[i] != '\0'; i++)
 		str[i] = s1[i];
 	for (j = 0; s2[j] != '\0'; j++)
