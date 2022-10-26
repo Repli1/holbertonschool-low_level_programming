779d260ab56bb92dd072d3baa518e4b386317c8e
Repli1 <5724@holbertonstudents.com>
2022-10-26 09:54:08 -0700

    fix2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,11 +12,16 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *array = malloc(size * nmemb);
+	char *array;
 
 	unsigned int i;
 
-	if (nmemb == 0 || size == 0 || array == NULL)
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	array = malloc(size * nmemb);
+
+	if (array == NULL)
 		return (NULL);
 
 	for (i = 0; i < (size * nmemb); i++)
