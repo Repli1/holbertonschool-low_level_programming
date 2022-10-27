fb67a54ec6be4134ad1ec1536c16958b488fad0c
Repli1 <5724@holbertonstudents.com>
2022-10-27 06:49:09 -0700

    added files

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog_h
+#define dog_h
+#include <unistd.h>
+/**
+ * struct dog - A dog structure.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Name of the dog's owner.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
