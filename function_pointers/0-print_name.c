add40abb023ae8391c81304aab587945cdc4535e
Repli1 <5724@holbertonstudents.com>
2022-10-29 07:47:43 -0700

    added files

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Entry point
+ *
+ * @name: A name.
+ *
+ * @f: A pointer to a function.
+ */
+
+void print_name(char *name, void (*f)(char *namefunc))
+{
+	namefunc(name);
+}
