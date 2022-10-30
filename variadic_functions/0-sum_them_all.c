5b39af655c2198b546da98f35499ededfcbce9b1
Repli1 <5724@holbertonstudents.com>
2022-10-30 05:52:13 -0700

    added files

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Entry point
+ *
+ * @n: The number of integers to be summed.
+ *
+ * Return: The sum.
+ */
+
+int sum_them_all(const unsigned int n, ...);
+{
+	va_list args;
+
+	va_start(args, n);
+
+	int i;
+
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	for (i = 0; i < n; i++)
+		sum = sum + va_arg(args, int);
+
+	va_end(args);
+
+	return (sum);
+}
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#include <unistd.h>
+#ifndef variadic_functions_h
+#define variadic_functions_h
+#endif
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
