a489bdf390204300cc59125b9274824d4af80e66
Repli1 <5724@holbertonstudents.com>
2022-11-14 08:56:20 -0800

    Added comments

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,7 +1,11 @@
 #include "lists.h"
 #include <stdio.h>
 
-
+/**
+ * print_list - Function that prints a singly linked list.
+ * @h: The first node of the list.
+ * Return: The number of nodes of the list.
+ */
 
 
 
@@ -15,7 +19,7 @@ size_t print_list(const list_t *h)
 			printf("[%d] (nil)\n", 0);
 		else
 		{
-			printf("[%d] ",h->len);
+			printf("[%d] ", h->len);
 			printf("%s\n", h->str);
 		}
 		i++;
