ddbcb62a2760553919f21d5efa2a707393585e8e
Repli1 <5724@holbertonstudents.com>
2022-11-15 08:33:53 -0800

    fixed malloc return

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -7,6 +7,10 @@ list_t *add_node(list_t **head, const char *str)
 	char *s = strdup(str);
 	list_t *p = malloc(sizeof(list_t));
 
+	if (p == NULL)
+		free(p);
+	if (s == NULL)
+		free(s);
 	p->str = s;
 	p->len = strlen(s);
 	p->next = *head;
