6a6cd85977b928d72621c8ec489745ae0a509333
Repli1 <5724@holbertonstudents.com>
2022-11-20 17:02:46 -0800

    Added files

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_nodeint_at_index - Creates a new node at the index given.
+ * @head: The first node of the list.
+ * @idx: The index to insert the node at.
+ * @n: The node's contents.
+ * Return: The direction of the new node.
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *p = *head;
+	listint_t *new = malloc(sizeof(listint_t));
+
+	new->n = n;
+	new->next = NULL;
+	if (head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	for (i = 0; i < idx; i++)
+		if (i != idx - 1)
+			p = p->next;
+	if (p->next == NULL)
+		return (NULL);
+	new->next = p->next;
+	p->next = new;
+	p = p->next;
+	p = p->next;
+	new->next = p;
+	return (new);
+}
