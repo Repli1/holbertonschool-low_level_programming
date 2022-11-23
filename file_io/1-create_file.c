ffa0c7044f93fb9ede7e71b135940f4dafd5f05e
Repli1 <5724@holbertonstudents.com>
2022-11-23 07:42:46 -0800

    Betty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -2,13 +2,18 @@
 #include <unistd.h>
 
 /**
- * create_file
+ * create_file - A function that creates a file with text.
+ * @filename: Name of the file to be created.
+ * @text_content: Content of the file to be created.
+ * Return: 1 if success, -1 if catastrophe.
  */
 
 int create_file(const char *filename, char *text_content)
 {
 	int fd, count = 0, w;
 
+	if (text_content == NULL)
+		text_content = "";
 	while (text_content[count] != '\0')
 		count++;
 	if (filename == NULL)
@@ -16,8 +21,6 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (text_content == NULL)
-		text_content = "";
 	w = write(fd, text_content, count);
 	if (w == -1)
 		return (-1);
