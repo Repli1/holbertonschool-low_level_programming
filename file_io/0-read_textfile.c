20e31a0b46ffaeec818cd4a2672e433736911fe5
Repli1 <5724@holbertonstudents.com>
2022-11-23 05:19:30 -0800

    fix2

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -13,13 +13,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buf = malloc(sizeof(char) * letters);
 	int fd, w, r;
 
-	if (filename == NULL)
-		return (0);
 	if (buf == NULL)
-		return (0);
+		return (-1);
 	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 	r = read(fd, buf, letters);
+	if (r == -1)
+		return (0);
 	w = write(1, buf, r);
+	if (w == -1)
+		return (0);
 	close(fd);
 	free(buf);
 	return (w);
