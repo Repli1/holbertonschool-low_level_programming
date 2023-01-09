dbe7c3355e754f06b5022f9a30bca7905f684178
Repli1 <5724@holbertonstudents.com>
2023-01-09 04:05:02 -0800

    fix

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -9,11 +9,11 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+	size_t i;
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%i] = [%i]\n", i, array[i]);
+		printf("Value checked array[%ld] = [%i]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
